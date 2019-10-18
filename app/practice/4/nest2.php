a731d97cd620ee01fafad24130636deeaba100c5
shoutakeshita <[agasa2003@gmail.com]>
2019-10-18 19:22:19 +0900

    nest2

---
+++ app/practice/4/nest2.php
@@ -0,0 +1,16 @@
+<?php
+
+$attend = 1;
+//欠席は0 出席は1
+$place ='b';
+
+if($attend === 0){
+    echo 'パーティを欠席にて承りました。';
+}elseif($attend === 1){
+    echo 'パーティを出席にて承りました。';
+    if($place === 'a'){
+        echo '会場はホテルでございます。';
+    }elseif($place === 'b'){
+        echo '会場はB広場でございます。';
+    }
+}
